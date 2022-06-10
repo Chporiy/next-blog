02fdfe1675d46ee784ff7795bccf1382925cd8d9
Chporiy <vostok43kull@yandex.ru>
2022-06-10 21:46:28 +0300

    Added <PostForm /> component

---
+++ src/components/PostForm.tsx
@@ -0,0 +1,53 @@
+import React, { FormEvent, useCallback, useState } from "react";
+
+const PostForm = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onTitleChange = useCallback(
+    ({ currentTarget }: FormEvent<HTMLInputElement>) => {
+      setTitle(currentTarget.value);
+    },
+    [setTitle]
+  );
+  const onBodyChange = useCallback(
+    ({ currentTarget }: FormEvent<HTMLTextAreaElement>) => {
+      setBody(currentTarget.value);
+    },
+    [setBody]
+  );
+  const onSubmit = useCallback(async () => {
+    if ([title, body].includes("")) return; // =>
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        body,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    setTitle("");
+    setBody("");
+  }, [title, body, setTitle, setBody]);
+
+  return (
+    <form className="postForm">
+      <input type="text" name="title" value={title} onChange={onTitleChange} />
+      <textarea
+        className="postForm-text"
+        name="body"
+        value={body}
+        onChange={onBodyChange}
+      />
+      <button type="button" onClick={onSubmit}>
+        submit
+      </button>
+    </form>
+  );
+};
+
+export default PostForm;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,37 @@
-import type { GetStaticProps } from 'next'
-import { ReactElement } from 'react'
-import Layout from '../components/Layout'
-import PostList from '../components/PostsList'
-import { Post } from '../interfaces'
-import { NextPageWithLayout } from './types'
+import type { GetStaticProps } from "next";
+import { ReactElement } from "react";
+import Layout from "../components/Layout";
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostsList";
+import { Post } from "../interfaces";
+import { NextPageWithLayout } from "./types";
 
 type Props = {
-  posts: Post[]
-}
+  posts: Post[];
+};
 
 const Index: NextPageWithLayout<Props> = ({ posts }) => {
   return (
     <div>
       <PostList posts={posts} />
+      <PostForm />
     </div>
-  )
-}
+  );
+};
 
-Index.getLayout = function getLayout(page : ReactElement) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
-}
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
 
-export const getStaticProps: GetStaticProps = async() => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts: Post[] = (await response.json()).slice(0, 10);
 
   return {
     props: {
-      posts
-    }
-  }
-}
+      posts,
+    },
+  };
+};
 
-export default Index
+export default Index;
