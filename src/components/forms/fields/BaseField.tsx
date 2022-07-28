f0b9d32ee14fa284f649a75e77bcf8ffa5f6fbab
Chporiy <arinov.e@yandex.ru>
2022-07-28 22:57:50 +0300

    Create a base field for Formik with chakra-ui components

---
+++ src/components/forms/fields/BaseField.tsx
@@ -0,0 +1,46 @@
+import {
+  FormControl,
+  FormControlProps,
+  FormErrorMessage,
+  FormLabel,
+} from '@chakra-ui/react';
+import React from 'react';
+import { FieldProps, getIn } from 'formik';
+
+/* export type BaseFieldProps = FieldProps &
+  Omit<InputProps, 'name' | 'value' | 'error'> & {
+    label: string;
+    disabled: boolean;
+  }; */
+export type BaseFieldProps = FieldProps & FormControlProps;
+
+/**
+ * A chakra-ui form control with FormLabel and ErrorMessage
+ * FormControl bound with Formik and uses as base for others fields
+ *
+ * @param {BaseFieldProps} props - Formik <Field /> props with Chakra-ui FormControlProps
+ */
+const BaseField = (props: BaseFieldProps) => {
+  const {
+    field,
+    isDisabled,
+    form: { errors, touched, isSubmitting },
+    label,
+    children,
+  } = props;
+  const error = getIn(errors, field.name);
+  const touch = getIn(touched, field.name);
+
+  return (
+    <FormControl
+      isInvalid={error && touch}
+      isDisabled={isDisabled ?? isSubmitting}
+    >
+      <FormLabel>{label}</FormLabel>
+      {children}
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default BaseField;
