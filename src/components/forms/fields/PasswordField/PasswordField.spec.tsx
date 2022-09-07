6b0793fb935d39c52db643dbcd2e7ea4f8b9664a
Chporiy <arinov.e@yandex.ru>
2022-09-07 23:30:44 +0300

    Create a password field for Formik with chakra-ui components

---
+++ src/components/forms/fields/PasswordField/PasswordField.spec.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import { Field, Form, Formik } from 'formik';
+import renderWithStore from '../../../../../tests/utils/renderWithStore';
+import PasswordField from './PasswordField';
+
+describe('<PasswordField>', () => {
+  it('should switch an input type', async () => {
+    const { user } = renderWithStore(
+      <Formik initialValues={{ password: '' }} onSubmit={jest.fn()}>
+        <Form>
+          <Field component={PasswordField} name="password" label="password" />
+        </Form>
+      </Formik>,
+    );
+
+    const button = screen.getByRole('button');
+    const field = screen.getByLabelText('password') as HTMLInputElement;
+
+    await user.click(button);
+
+    expect(field.type).toEqual('text');
+    expect(screen.getByText('hide')).toBeInTheDocument();
+
+    await user.click(button);
+
+    expect(field.type).toEqual('password');
+    expect(screen.getByText('show')).toBeInTheDocument();
+  });
+});
