4d772774aeef6815ae788c626e1cc4264e2cc497
Aken <arinov.e@yandex.ru>
2023-08-08 17:29:42 +0300

    Update <PostPreview />

--- src/features/posts/PostPreview/PostPreview.tsx
+++ src/features/posts/PostPreview/PostPreview.tsx
@@ -1,22 +1,22 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
 import NextLink from '../../../components/links/NextLink';
 import PostAuthor from '../PostAuthor/PostAuthor';
 import PostTitle from '../PostTitle/PostTitle';
 import { Post } from '../types';
 import ROUTES from '../../../utils/routes/routes';
+import ContentWrapper from '../../../components/layout/ContentWrapper/ContentWrapper';
 
 type Props = {
   post: Post;
 };
 
 const PostPreview = ({ post }: Props) => (
-  <Box p={5} borderWidth="1px" borderRadius="md" bg="white" shadow="xs">
+  <ContentWrapper p="4" alignItems="flex-start">
     <PostAuthor userId={post.userId} date={post.date} />
     <NextLink next={{ href: `${ROUTES.posts.id}/${post.id}` }}>
       <PostTitle marginTop="3">{post.title}</PostTitle>
     </NextLink>
-  </Box>
+  </ContentWrapper>
 );
 
 export default PostPreview;
