f6f70a0e2882b2c9cea63eb888562675593ee165
Aken <arinov.e@yandex.ru>
2023-08-08 16:46:42 +0300

    Update <PostAuthor />

--- src/features/posts/PostAuthor/PostAuthor.tsx
+++ src/features/posts/PostAuthor/PostAuthor.tsx
@@ -1,9 +1,10 @@
 import { Avatar, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import getPostDate from '../../../utils/getPostDate/getPostDate';
-import { selectUserById } from '../../users/selectors';
-import { useGetUsersQuery } from '../../users/usersApi';
+import { useGetUserQuery } from '../../users/usersApi';
 import { Post } from '../types';
+import UserAvatar from '../../users/UserAvatar/UserAvatar';
+import UserFullname from '../../users/UserFullname/UserFullname';
 
 type Props = Pick<Post, 'date' | 'userId'>;
 
@@ -13,17 +14,13 @@ type Props = Pick<Post, 'date' | 'userId'>;
  * Render error text and 'no user' avatar if iser is`nt defined
  */
 const PostAuthor = ({ userId, date }: Props) => {
-  const { user } = useGetUsersQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      user: selectUserById(data, userId),
-    }),
-  });
+  const { data: user } = useGetUserQuery(userId);
 
   return user ? (
     <Flex>
-      <Avatar mr="3" src={user.avatar} name={user.fullName} />
+      <UserAvatar mr="3" user={user} />
       <Flex direction="column">
-        <Text fontWeight="medium">{user.fullName}</Text>
+        <UserFullname user={user} fontWeight="medium" />
         <Text color="gray.400" fontSize="sm">
           {getPostDate(date)}
         </Text>
--- src/features/users/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './types';
-
-// eslint-disable-next-line import/prefer-default-export
-export const selectUserById = (data: User[], id: number): User | undefined =>
-  data?.find((user) => user.id === id);
