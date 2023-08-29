94571aa3be3a28ff0cf999ba4b531c3adeaddd82
Aken <arinov.e@yandex.ru>
2023-08-29 22:17:07 +0300

    Add <List />

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export * from './content-wrapper/ContentWrapper';
 export * from './fields';
 export * from './layout/Layout';
 export * from './next-link/NextLink';
+export * from './list';
---
+++ src/shared/ui/list/List.tsx
@@ -0,0 +1,19 @@
+import { List as ChakraList, ListItem } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+interface Item {
+  id: number;
+}
+
+interface Props<T extends Item> {
+  items: T[];
+  children(item: T): ReactNode;
+}
+
+export const List = <T extends Item>({ items, children }: Props<T>) => (
+  <ChakraList spacing="4">
+    {items.map((item) => (
+      <ListItem key={item.id}>{children(item)}</ListItem>
+    ))}
+  </ChakraList>
+);
