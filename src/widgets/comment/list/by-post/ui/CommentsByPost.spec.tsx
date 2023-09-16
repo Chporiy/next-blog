f36998e79579d5a875fd591357492d856716aa53
Aken <arinov.e@yandex.ru>
2023-09-16 18:56:17 +0300

    Render <CommentsByComemnt /> under primary comment

--- src/widgets/comment/list/by-post/ui/CommentsByPost.spec.tsx
+++ src/widgets/comment/list/by-post/ui/CommentsByPost.spec.tsx
@@ -1,19 +1,41 @@
 import { postMock0 } from '~/tests/mocks';
-import { render, screen } from '~/tests/utils';
-import { getCommentsByPostForTest } from '~/tests/utils/get-comments-by-post-for-test/getCommentsByPostForTest';
+import { render, screen, waitFor } from '~/tests/utils';
+import { getCommentsByCommentsForTest } from '~/tests/utils/get-comments-by-comments-for-test/getCommentsByCommentsForTest';
+import { getPrimaryCommentsByPostForTest } from '~/tests/utils/get-primary-comments-by-post-for-test/getPrimaryCommentsByPostForTest';
 
 import { List } from './CommentsByPost';
 
 describe('<CommentsByPost />', () => {
-  it('should render comments by a post', () => {
+  it('should render primary comments by a post', async () => {
     render(<List postId={postMock0.id} />);
 
-    const commentsByPost = getCommentsByPostForTest(postMock0.id);
+    const commentsByPost = getPrimaryCommentsByPostForTest(postMock0.id);
 
-    commentsByPost.forEach(async (comment) => {
-      const element = await screen.findByText(comment.body);
+    await waitFor(async () => {
+      commentsByPost.forEach((comment) => {
+        const element = screen.getByText(comment.body);
 
-      expect(element).toBeInTheDocument();
+        expect(element).toBeInTheDocument();
+      });
+    });
+  });
+
+  it('should render an inner comments list if the list slot is passed as a prop', async () => {
+    render(<List postId={postMock0.id} />);
+
+    const postCommentsWithInnerComments = getPrimaryCommentsByPostForTest(
+      postMock0.id,
+    ).find((comment) => comment.childrenCommentsAmount !== 0);
+    const innerComments = getCommentsByCommentsForTest(
+      postCommentsWithInnerComments.id,
+    );
+
+    await waitFor(() => {
+      innerComments.forEach((comment) => {
+        const innerCommentBody = screen.getByText(comment.body);
+
+        expect(innerCommentBody).toBeInTheDocument();
+      });
     });
   });
 });
--- src/widgets/comment/list/by-post/ui/CommentsByPost.tsx
+++ src/widgets/comment/list/by-post/ui/CommentsByPost.tsx
@@ -1,24 +1,36 @@
-import { commentModel, useGetCommentsByPostQuery } from '~/entities/comment';
+import {
+  commentModel,
+  useGetPrimaryCommentsByPostQuery,
+} from '~/entities/comment';
 
 import { Post } from '~/shared/model';
 
 import { BaseCommentList } from '../../base';
+import { CommentsByComment } from '../../by-comment';
 
 interface Props {
   postId: Post['id'];
 }
 
 export const List = ({ postId }: Props) => {
-  const { data: comments, isSuccess } = useGetCommentsByPostQuery(postId, {
-    selectFromResult: (result) => ({
-      ...result,
-      data: commentModel.selectors.sortCommentsByDescDate(result),
-    }),
-  });
+  const { data: comments, isSuccess } = useGetPrimaryCommentsByPostQuery(
+    postId,
+    {
+      selectFromResult: (result) => ({
+        ...result,
+        data: commentModel.selectors.sortCommentsByDescDate(result),
+      }),
+    },
+  );
 
   if (!isSuccess) {
     return null;
   }
 
-  return <BaseCommentList comments={comments} />;
+  return (
+    <BaseCommentList
+      comments={comments}
+      slots={{ innerCommentsList: CommentsByComment }}
+    />
+  );
 };
