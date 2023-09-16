43d4a43287854e0d42146a85c4fe8b925a9a8a4d
Aken <arinov.e@yandex.ru>
2023-09-16 18:52:58 +0300

    Update <CommentCard />

--- src/features/comment/card/ui/CommentCard.spec.tsx
+++ src/features/comment/card/ui/CommentCard.spec.tsx
@@ -1,10 +1,13 @@
 import { commentMock0, userMock0 } from '~/tests/mocks';
 import { render, screen } from '~/tests/utils';
+import { getCommentsByCommentsForTest } from '~/tests/utils/get-comments-by-comments-for-test/getCommentsByCommentsForTest';
+
+import { List } from '~/shared/ui';
 
 import { Card } from './CommentCard';
 
 describe('<CommentCard>', () => {
-  it('should have a comment author', async () => {
+  it('should render a comment author', async () => {
     render(<Card comment={commentMock0} />);
 
     const author = await screen.findByText(userMock0.fullName);
@@ -18,5 +21,26 @@ describe('<CommentCard>', () => {
     const body = screen.getByText(commentMock0.body);
 
     expect(body).toBeInTheDocument();
+  });
+
+  it('should render an inner comments list if the list slot is passed as a prop', () => {
+    const innerComments = getCommentsByCommentsForTest(commentMock0.id);
+
+    render(
+      <Card
+        comment={commentMock0}
+        slots={{
+          commentsList: () => (
+            <List items={innerComments}>{(comment) => comment.body}</List>
+          ),
+        }}
+      />,
+    );
+
+    innerComments.forEach((comment) => {
+      const innerCommentBody = screen.getByText(comment.body);
+
+      expect(innerCommentBody).toBeInTheDocument();
+    });
   });
 });
--- src/features/comment/card/ui/CommentCard.tsx
+++ src/features/comment/card/ui/CommentCard.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 import { Comment } from '~/shared/model';
 import { ContentWrapper } from '~/shared/ui';
@@ -7,11 +7,22 @@ import { CommentAuthor } from '../../author';
 
 type Props = {
   comment: Comment;
+  slots?: {
+    commentsList?: (...args: any[]) => JSX.Element;
+  };
 };
 
-export const Card = ({ comment }: Props) => (
-  <ContentWrapper align="start" p="4">
-    <CommentAuthor comment={comment} />
-    <Text mt="3">{comment.body}</Text>
-  </ContentWrapper>
+export const Card = ({
+  comment,
+  slots = { commentsList: () => null },
+}: Props) => (
+  <>
+    <ContentWrapper align="start" p="4">
+      <CommentAuthor comment={comment} />
+      <Text mt="3">{comment.body}</Text>
+    </ContentWrapper>
+    <Box mt="4" pl="4">
+      <slots.commentsList commentId={comment.id} />
+    </Box>
+  </>
 );
